6cdd10d69a102352d0208843c6834445ee9c7e10
subhranshudas <subhranshu.opt@gmail.com>
2022-08-05 09:07:18 +0530

    working RNYT but fails with other error

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// import { Notification } from '@dasubh/uimobile';
-import YouTube from 'react-native-youtube';
+import { Notification } from '@dasubh/uimobile';
+// import YouTube from 'react-native-youtube';
 
 import { DEFAULT_NOTIFICATIONS } from './notificationData';
 import { parseApiResponse } from './helpers';
@@ -10,15 +10,17 @@ import { parseApiResponse } from './helpers';
 export default function App() {
   const notifData = parseApiResponse(DEFAULT_NOTIFICATIONS);
 
-  // AIzaSyBrzkFPyNmVDFzGY7dKz2HocUO4m-ni-Fc
+  const lobCb = (args) => {
+    console.log('logs stuff', args);
+  }
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
 
-       {/* {notifData.map(
+       {notifData.map(
           (oneNotification, idx) => {
-            const {cta, title, message, app, icon, image} = oneNotification;
+            const {cta, title, message, app, icon, image, blockchain } = oneNotification;
 
             return (
               <Notification
@@ -29,23 +31,24 @@ export default function App() {
                 app={app}
                 icon={icon}
                 image={image}
+                chainName={blockchain}
               />
             );
           }
-        )} */}
+        )}
 
-        <YouTube
+        {/* <YouTube
           apiKey="AIzaSyBrzkFPyNmVDFzGY7dKz2HocUO4m-ni-Fc"
           videoId="KVZ-P-ZI6W4" // The YouTube video ID
           play // control playback of video with true/false
           fullscreen // control whether the video should play in fullscreen or inline
           loop // control whether the video should loop when ended
-          onReady={e => this.setState({ isReady: true })}
-          onChangeState={e => this.setState({ status: e.state })}
-          onChangeQuality={e => this.setState({ quality: e.quality })}
-          onError={e => this.setState({ error: e.error })}
+          onReady={lobCb}
+          onChangeState={lobCb}
+          onChangeQuality={lobCb}
+          onError={lobCb}
           style={{ alignSelf: 'stretch', height: 300 }}
-        />
+        /> */}
 
       <StatusBar style="auto" />
     </View>
